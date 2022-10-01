e2a29cb72698ef0ec7f9275d910b6a6f988ba05c
fans2619 <fans2619@gmail.com>
2022-10-02 03:47:38 +0800

    Handle leave room in matching service

--- matching-service/index.js
+++ matching-service/index.js
@@ -5,7 +5,7 @@ import { db } from './model/database.js';
 import { Server } from 'socket.io';
 
 import { createServer } from 'http';
-import { handleNewMatch } from './controller/match-controller.js';
+import { handleEnterRoom, handleLeaveRoom, handleNewMatch } from './controller/match-controller.js';
 
 const app = express();
 const port = process.env.PORT || 8001;
@@ -23,6 +23,7 @@ await db.sync().then(() => {
 });
 
 const server = createServer(app);
+// TODO: Add CORS
 const io = new Server(server);  // TODO: export does not seem to be the best practice
 
 io.of('/api/match')
@@ -30,12 +31,26 @@ io.of('/api/match')
     console.log('a user connected, socket id: ' + socket.id); // ojIckSD2jqNzOqIrAGzL
 
     socket.on('new match', (data) => {
+      // emitted when user clicks a button to start looking for a new match
       handleNewMatch(data, socket);
     });
 
-    // TODO: handle leave room
+    socket.on('enter room', (data) => {
+      // emitted when user enters the room (page) after matching
+      handleEnterRoom(data, socket);
+    });
 
-    // TODO: handle disconnection
+    // TODO: handle leave room
+    socket.on('leave room', (data) => {
+      // emitted when user clicks a button to end the session
+      handleLeaveRoom(data, socket);
+    });
+
+    socket.on('disconnect', () => {
+      // emitted when client calls socket.disconnect() at the end (after session ends and socket is no longer used)
+      console.log('a user disconnected, socket id: ' + socket.id);
+      // TODO: handle disconnect (This may be a rare case)
+    });
   });
 
 server.listen(port, () => {
--- matching-service/model/repository.js
+++ matching-service/model/repository.js
@@ -1,24 +1,29 @@
 import { EasyPendingMatch, HardPendingMatch, MediumPendingMatch } from './match-model.js';
+import { Room } from './room-model.js';
 
-export async function createPendingMatch(userId, username, difficultyLevel, roomId) {
+export async function createPendingMatch(username, difficultyLevel, roomId) {
   switch (difficultyLevel) {
     case 'easy':
-      return EasyPendingMatch.create({ userId, username, roomId });
+      return EasyPendingMatch.create({ username, roomId });
     case 'medium':
-      return MediumPendingMatch.create({ userId, username, roomId });
+      return MediumPendingMatch.create({ username, roomId });
     case 'hard':
-      return HardPendingMatch.create({ userId, username, roomId });
+      return HardPendingMatch.create({ username, roomId });
+    default:
+      throw new Error('Invalid difficulty level');
   }
 }
 
-export async function deletePendingMatch(userId, difficultyLevel) {
+export async function deletePendingMatch(username, difficultyLevel) {
   switch (difficultyLevel) {
     case 'easy':
-      return EasyPendingMatch.destroy({ where: { userId } });
+      return EasyPendingMatch.destroy({ where: { username } });
     case 'medium':
-      return MediumPendingMatch.destroy({ where: { userId } });
+      return MediumPendingMatch.destroy({ where: { username } });
     case 'hard':
-      return HardPendingMatch.destroy({ where: { userId } });
+      return HardPendingMatch.destroy({ where: { username } });
+    default:
+      throw new Error('Invalid difficulty level');
   }
 }
 
@@ -30,5 +35,23 @@ export async function findSameLevelPendingMatch(difficultyLevel) {
       return MediumPendingMatch.findOne();
     case 'hard':
       return HardPendingMatch.findOne();
+    default:
+      throw new Error('Invalid difficulty level');
   }
+}
+
+export async function createRoom(username, roomId, difficultyLevel) {
+  if (difficultyLevel === 'easy' || difficultyLevel === 'medium' || difficultyLevel === 'hard') {
+    return Room.create({ username, roomId, difficultyLevel });
+  } else {
+    throw new Error('Invalid difficulty level');
+  }
+}
+
+export async function deleteRoomByRoomId(roomId) {
+  return Room.destroy({ where: { roomId } });
+}
+
+export async function getRoomByUsername(username) {
+  return Room.findByPk(username);
 }
