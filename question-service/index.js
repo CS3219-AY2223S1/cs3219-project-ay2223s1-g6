95a0d8482319c0b012676a09843d3c093d07360b
Yaoren Xie <xie.yaoren@bytedance.com>
2022-10-09 04:52:49 +0800

    refactor codes

--- question-service/index.js
+++ question-service/index.js
@@ -1,12 +1,14 @@
 // Import express
-let express = require('express');
+//let express = require('express');
+import express from 'express'
 // Import Body parser
-let bodyParser = require('body-parser');
+//let bodyParser = require('body-parser');
+import bodyParser from 'body-parser'
 // Import Mongoose
-let mongoose = require('mongoose');
+//let mongoose = require('mongoose');
 // Initialise the app
 let app = express();
-require("dotenv").config();
+//require("dotenv").config();
 
 // Configure bodyparser to handle post requests
 app.use(bodyParser.urlencoded({
@@ -16,18 +18,21 @@ app.use(bodyParser.json());
 
 
 // Import routes
-let apiRoutes = require("./question_routers/questions_api");
+import router from "./question_routers/questions_api.js";
+//let apiRoutes = require("./question_routers/questions_api.js");
+// // Connect to Mongoose and set connection variable
+// console.log(process.env.MONGODB_URI)
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/questionBank', { useNewUrlParser: true});
+// var db = mongoose.connection;
+//
+// // Added check for DB connection
+// if(!db)
+//     console.log("Error connecting db")
+// else
+//     console.log("Db connected successfully")
 
-// Connect to Mongoose and set connection variable
-console.log(process.env.MONGODB_URI)
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/questionBank', { useNewUrlParser: true});
-var db = mongoose.connection;
-
-// Added check for DB connection
-if(!db)
-    console.log("Error connecting db")
-else
-    console.log("Db connected successfully")
+//import {router} from './question_routers/questions_api';
+// import { createUser } from './controllers/question-controller';
 
 // Setup server port
 var port = 4002 || process.env.PORT;
@@ -36,7 +41,7 @@ var port = 4002 || process.env.PORT;
 app.get('/', (req, res) => res.send('Hello World with Express'));
 
 //Use Api routes in the App
-app.use('/questions', apiRoutes);
+app.use('/questions', router);
 // Launch app to listen to specified port
 console.log(port)
 app.listen(port, function () {
--- question-service/question_routers/questions_api.js
+++ question-service/question_routers/questions_api.js
@@ -1,6 +1,8 @@
 // api-routes.js
 // Initialize express router
-let router = require('express').Router();
+//let router = require('express').Router();
+import express from 'express'
+let router = express.Router()
 // Set default API response
 router.get('/', function (req, res) {
     res.json({
@@ -10,14 +12,28 @@ router.get('/', function (req, res) {
 });
 
 // Import contact controller
-var questionsController = require('../controllers/questionControllers');
+//var questionsController = require('../controllers/questionControllers.js');
+//var questionsController1 = require('../controllers/question-controller');
+import {
+    createUser as _createUser,
+    getRandomQuestionNo as _getRandomQuestionNo,
+    viewQuestion as _viewQuestion,
+    updateQuestion as _updateQuestion,
+    deleteQuestion as _deleteQuestion
+} from "../controllers/question-controller.js";
+// var abc = require('../controllers/question-controller');
 
 // create contact
-router.route('/add').post(questionsController.new)
-router.route('/delete/:question_id?').delete(questionsController.delete)
-router.route('/update/:question_id?').put(questionsController.update)
-router.route('/read/:question_id?').get(questionsController.view)
-router.route('/readRandom/:difficultyLevel?').get(questionsController.getRandomQuestionNo)
+// router.route('/add').post(questionsController.new)
+// router.route('/delete/:question_id?').delete(questionsController.delete)
+// router.route('/update/:question_id?').put(questionsController.update)
+// router.route('/read/:question_id?').get(questionsController.view)
+// router.route('/readRandom/:difficultyLevel?').get(questionsController.getRandomQuestionNo)
+router.route('/').post(_createUser)
+router.route('/:difficultyLevel?').get(_getRandomQuestionNo)
+router.route('/:question_id?').get(_viewQuestion)
+router.route('/:question_id?').put(_updateQuestion)
+router.route('/:question_id?').delete(_deleteQuestion)
 
-
-module.exports = router;
+export default router;
+//module.exports = router;
