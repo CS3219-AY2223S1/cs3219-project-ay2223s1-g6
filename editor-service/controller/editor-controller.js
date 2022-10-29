e62c71d2ee5d61033938d0c08a8ebeac02e4d1c8
fans2619 <fans2619@gmail.com>
2022-10-30 00:00:55 +0800

    Add editor service

---
+++ editor-service/controller/editor-controller.js
@@ -0,0 +1,72 @@
+import axios from 'axios';
+import { joinRoom, newChange } from '../service/editor-service.js';
+
+async function authenticateUser(username, token) {
+  // TODO: reference config file for user service url
+  let authSuccess;
+  await axios.get('http://localhost:8000/api/user/authentication', {
+    params: {
+      username: username,
+      auth: token,
+    },
+  }).then(() => {
+    authSuccess = true;
+  }).catch(() => {
+    authSuccess = false;
+  });
+  return authSuccess;
+}
+
+export async function handleJoinRoom(data, socket) {
+  try {
+    const { token, username, roomId } = data;
+    if (token && username && roomId) {
+      const authSuccess = await authenticateUser(username, token);
+      if (!authSuccess) {
+        socket.emit('join room failure', {
+          status: 403,
+          message: 'Not allowed to access this resource',
+        });
+        return;
+      }
+      joinRoom(roomId, socket);
+    } else {
+      socket.emit('join room failure', {
+        status: 400,
+        message: 'Missing token and/or username and/or roomId',
+      });
+    }
+  } catch (err) {
+    socket.emit('join room failure', {
+      status: 500,
+      message: `The server encounters an error: ${err}`,
+    });
+  }
+}
+
+export async function handleEditorChange(data, socket) {
+  try {
+    const { token, username, newContent } = data;
+    if (token && username && newContent) {
+      const authSuccess = await authenticateUser(username, token);
+      if (!authSuccess) {
+        socket.emit('change failure', {
+          status: 403,
+          message: 'Not allowed to access this resource',
+        });
+        return;
+      }
+      newChange(username, newContent, socket);
+    } else {
+      socket.emit('change failure', {
+        status: 400,
+        message: 'Missing token and/or username and/or newContent',
+      });
+    }
+  } catch (err) {
+    socket.emit('change failure', {
+      status: 500,
+      message: `The server encounters an error: ${err}`,
+    });
+  }
+}
---
+++ editor-service/index.js
@@ -0,0 +1,42 @@
+import cors from 'cors';
+import 'dotenv/config';
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import { handleEditorChange, handleJoinRoom } from './controller/editor-controller.js';
+
+const app = express();
+const port = process.env.PORT || 8003;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors()); // config cors so that front-end can use
+app.options('*', cors());
+
+const server = createServer(app);
+const io = new Server(server, { cors: { origin: '*' } });
+
+io.of('/api/editor')
+  .on('connection', (socket) => {
+    console.log('a user connected, socket id: ' + socket.id); // ojIckSD2jqNzOqIrAGzL
+
+    socket.on('join room', (data) => {
+      // emitted when user enters the room (page) after matching
+      handleJoinRoom(data, socket);
+    });
+
+    socket.on('change', (data) => {
+      // emitted when either user sends a new change
+      handleEditorChange(data, socket);
+    });
+
+    socket.on('disconnect', () => {
+      // nothing to handle
+    });
+  });
+
+server.listen(port, () => {
+  console.log('Editor service server started on port ' + port);
+});
+
+export default io;
