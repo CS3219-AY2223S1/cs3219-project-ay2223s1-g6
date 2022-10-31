38ebf2a818b441743f40f800083fcd555d930871
fans2619 <fans2619@gmail.com>
2022-11-01 00:30:00 +0800

    Authenticate user using token initially

--- frontend/src/components/contexts/AuthContext.js
+++ frontend/src/components/contexts/AuthContext.js
@@ -1,22 +1,65 @@
+import { Box, LinearProgress } from '@mui/material';
+import axios from 'axios';
 import Cookies from 'js-cookie';
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 export const AuthContext = createContext(null);
 
-export const AuthProvider = ({ children }) => {
-  const checkLoggedIn = () => {
-    // TODO: Ideally, user should be determined solely based on token
-    // TODO: Validate token
-    const uname = Cookies.get('username');
-    const token = Cookies.get('auth');
-    console.log('logged in function called in AuthContext!');
-    return !!(uname && token);
-  };
+const checkLoggedIn = async () => {
+  // TODO: Ideally, user should be determined solely based on token
+  const uname = Cookies.get('username');
+  const token = Cookies.get('auth');
+  const authSuccess = await authenticateUser(uname, token);
+  if (!authSuccess) {
+    Cookies.remove('username');
+    Cookies.remove('auth');
+    return false;
+  }
+  return true;
+};
 
+async function authenticateUser(username, token) {
+  if (!username || !token) {
+    return false;
+  }
+
+  // TODO: reference config file for user service url
+  let authSuccess;
+  await axios.get('http://localhost:8000/api/user/authentication', {
+    params: {
+      username: username,
+      auth: token,
+    },
+  }).then(() => {
+    authSuccess = true;
+  }).catch(() => {
+    authSuccess = false;
+  });
+  return authSuccess;
+}
+
+export const AuthProvider = ({ children }) => {
   // subsequent rerender using setLoggedIn will not trigger checkLoggedIn!
-  const [loggedIn, setLoggedIn] = useState(() => checkLoggedIn());
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [checking, setChecking] = useState(true);
+
+  useEffect(() => {
+    setChecking(true);
+    checkLoggedIn().then((result) => {
+      setLoggedIn(result);
+      setChecking(false);
+    });
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ loggedIn, setLoggedIn }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ loggedIn, setLoggedIn }}>
+      {
+        checking ?
+          <Box sx={{ width: '100%' }}>
+            <LinearProgress color="inherit"/>
+          </Box>
+          : children
+      }
+    </AuthContext.Provider>
   );
 };
